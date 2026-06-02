ef44addedf1b745b8c7027b099b4684606b145e7
yxshv <yashverma30115@gmail.com>
2026-06-02 12:09:01 +0530

    new stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
 const isUserSite = repoName?.endsWith('.github.io');
 
 export default defineConfig({
-  site: process.env.SITE_URL ?? 'https://username.github.io',
+  site: process.env.SITE_URL ?? 'https://yxsh.github.io/vault',
   base: process.env.BASE_PATH ?? (repoName && !isUserSite ? `/${repoName}` : undefined),
 });
